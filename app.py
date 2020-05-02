598b0a2700f943b4ceb23916df4af2bfb0c46da2
phemmylintry <phemmylintry@gmail.com>
2020-05-02 13:54:58 +0100

    imported yaml

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_mysqldb import MySQL
+import yaml
+
 
 app = Flask(__name__)
 Bootstrap(app)
+
+#configure db
+db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_host']
+app.config['MYSQL_PASSWORD'] = db['mysql_host']
+app.config['MYSQL_DB'] = db['mysql_host']
 
 @app.route('/')
 def hello_world():
