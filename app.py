e848d972da17cf2cdae5f4c78783f99728d68e9c
phemmylintry <phemmylintry@gmail.com>
2020-05-03 19:45:44 +0100

    flaskrr

--- app.py
+++ app.py
@@ -10,14 +10,23 @@ Bootstrap(app)
 #configure db
 db = yaml.load(open('db.yaml'))
 app.config['MYSQL_HOST'] = db['mysql_host']
-app.config['MYSQL_USER'] = db['mysql_host']
-app.config['MYSQL_PASSWORD'] = db['mysql_host']
-app.config['MYSQL_DB'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+mysql = MySQL(app)
+
 
 @app.route('/')
 def hello_world():
-    ls = ['mango', 'Apple', 'orange']
-    return render_template('index.html', ls=ls)
+    cur = mysql.connection.cursor()
+    # cur.execute("INSERT INTO user VALUES(%s)", ['Mike'])
+    # mysql.connection.commit()
+    result = cur.execute("SELECT * FROM user")
+    if result > 0:
+        users = cur.fetchall()
+        return users[1]
+    # ls = ['mango', 'Apple', 'orange']
+    return render_template('index.html')
     #return url_for('about')
 
 # @app.route('/about')
