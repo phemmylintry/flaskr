19e1f54c5178621aedbb750cd8b95199a9d7e68b
phemmylintry <phemmylintry@gmail.com>
2020-05-04 18:31:15 +0100

    flaskr

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from flask_bootstrap import Bootstrap
 from flask_mysqldb import MySQL
+from werkzeug.security import generate_password_hash
 import yaml
+import os
 
 
 app = Flask(__name__)
@@ -9,6 +11,7 @@ Bootstrap(app)
 
 #configure db
 db = yaml.load(open('db.yaml'))
+app.config['SECRET_KEY'] = os.urandom(24)
 app.config['MYSQL_HOST'] = db['mysql_host']
 app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
@@ -25,6 +28,7 @@ def index():
     #     return 'success', 201
     if request.method == 'POST':
         name = request.form['name']
+        name = generate_password_hash(name)
         email = request.form['email']
         cur = mysql.connection.cursor()
         cur.execute("INSERT INTO details(name, email) VALUES (%s, %s)", (name, email))
@@ -39,6 +43,7 @@ def about():
     result = cur.execute("SELECT * FROM details")
     if result > 0:
         details = cur.fetchall()
+        session['name'] = details[0]['name']
         return render_template('about.html', details=details)
 
 # @app.route('/css')
