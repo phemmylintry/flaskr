03b03879739db218511e3802e5d357dfd2660f00
phemmylintry <phemmylintry@gmail.com>
2020-05-04 11:17:53 +0100

    flaskrr

--- app.py
+++ app.py
@@ -13,32 +13,41 @@ app.config['MYSQL_HOST'] = db['mysql_host']
 app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DB'] = db['mysql_db']
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def hello_world():
+def index():
     # cur = mysql.connection.cursor()
-    # cur.execute("INSERT INTO user VALUES(%s)", ['Mike'])
-    # mysql.connection.commit()
-    # result = cur.execute("SELECT * FROM user")
-    # if result > 0:
-    #     users = cur.fetchall()
-    #     return users[1]
+    # if cur.execute("INSERT user(username) VALUES('Ben')"):
+    #     mysql.connection.commit()
+    #     return 'success', 201
     if request.method == 'POST':
-        return request.form['password']
-        # return 'Registered'
-    ls = ['mango', 'Apple', 'orange']
-    return render_template('index.html', ls=ls)
+        name = request.form['name']
+        email = request.form['email']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO details(name, email) VALUES (%s, %s)", (name, email))
+        mysql.connection.commit()
+    # ls = ['mango', 'Apple', 'orange']
+    return render_template('index.html')
     #return url_for('about')
 
-# @app.route('/about')
-# def about():
-#     return render_template('about.html')
+@app.route('/about')
+def about():
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * FROM details")
+    if result > 0:
+        details = cur.fetchall()
+        return render_template('about.html', details=details)
 
 # @app.route('/css')
 # def css():
 #     return render_template('css.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return 'Page Not Found'
 
 if __name__ == '__main__':
     app.run(debug=True)
